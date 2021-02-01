7b955324cbb2b9a23ed9d7d9e8b34536c1d60275
Jack Petersen <peterja7@os1.engr.oregonstate.edu>
2021-01-31 18:06:35 -0800

    reads everything in correctly. CD works, & works, blank lines and comments work.

--- smallsh.c
+++ smallsh.c
@@ -7,14 +7,29 @@
 int exitFlag = 0;
 struct input *getInput();
 struct input *parseInput(char * buffer);
+void cdCommand(struct input *currInput);
+void init(struct input *currInput);
 int main()
 {
 	struct input * currInput = getInput();
-    
-	while(strcmp(currInput->command, "exit") != 0)
+	   
+	while(exitFlag != 1)
 	{
+		//check to see if user input was blank or a comment
+		if(currInput->flag == 1)
+		{
+			currInput->flag = 0;
+			init(currInput);
+			getInput();
+		}
+		//user entered cd
+		else if(strncmp(currInput->commandArgc[0], "cd", 2) == 0)
+		{
+			cdCommand(currInput);
+		}
+		//get Input
+		init(currInput);
 		currInput = getInput();
-//		printf("%s", currInput->command);	
 	}
 }
 
@@ -22,7 +37,7 @@ int main()
 //This function parses through the input line
 struct input *parseInput(char * buffer)
 {
-	int i = 0;
+	int i = 1;
         if(strcmp(buffer, " ") == 0)
         {
                 getInput();
@@ -31,10 +46,9 @@ struct input *parseInput(char * buffer)
         char *savePtr = NULL;
 	//get command
         char * token = strtok_r(buffer, " ", &savePtr);
-	currInput->command = calloc(strlen(token)+1, sizeof(char));
-        strcpy(currInput->command, token);
+  	currInput->commandArgc[0] = strdup(token);
 	token = strtok_r(NULL, " ", &savePtr);
-//	printf("%s", currInput->command);
+//	printf("%s", token);
 	while(token != '\0')
 	{
 		if(strcmp(token, "<") == 0)
@@ -50,19 +64,30 @@ struct input *parseInput(char * buffer)
                         currInput->outFile = calloc(strlen(token)+1, sizeof(char));
                         strcpy(currInput->outFile, token);
                 }
+		//==================================================================
+		////testing only
+		//==================================================================
+		else if(strcmp(currInput->commandArgc[0], "exit") == 0)
+		{
+			exitFlag = 1;
+		}
+		//=================================================================
 		else
 		{
-			currInput->argc[i] = token;
+			currInput->commandArgc[i] = strdup(token);
 			i++;
 		}
 		token = strtok_r(NULL, " ", &savePtr);
 
 	}
-	printf("%s", currInput->inFile);
-	printf("\n");
-	printf("%s", currInput->argc[i]);
-
-
+	printf("%s", currInput->commandArgc[i-1]);
+	
+	//check if last argument is &
+	if(strcmp(currInput->commandArgc[i-1], "&\n") == 0)
+	{
+		currInput->ampersand = 1;
+		printf("Background Proccess");
+	}
 
         return currInput;
 }
@@ -70,17 +95,49 @@ struct input *parseInput(char * buffer)
 //gets input
 struct input *getInput()
 {
+	int pid = getpid();
         char *buffer = NULL;
         size_t len = 0;
         ssize_t nRead = 0;
         printf(": ");
+	fflush(stdout);
         nRead = getline(&buffer, &len, stdin);
+	struct input *currLine = parseInput(buffer);
+
 	if(buffer[0] == '#')
 	{
-		getInput();
+		currLine->flag = 1;
 	}
-        struct input *currLine = parseInput(buffer);
+	else if(strlen(buffer) == 1)
+	{
+		printf("BLANK LINE");
+		currLine->flag =1;
+	}
         return currLine;
 
 }
-
+// change directory command implementation
+void cdCommand(struct input * currInput)
+{
+	if(currInput->commandArgc[1] == NULL)
+	{
+		chdir(getenv("HOME"));
+		printf("hello");
+	}
+	else
+	{
+		chdir(currInput->commandArgc[1]);
+		printf("goodbye");
+		
+	}
+}
+// initialize all elements of the command and argument array to NULL
+void init(struct input * currInput)
+{
+	int i;
+	for(i = 0; i < 513; i++)
+	{
+		currInput->commandArgc[i] = NULL;
+		i++;
+	}
+}
---
+++ smallsh.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <time.h>
+
+struct input{
+	char *commandArgc[513];
+	char *inFile;
+	char * outFile;
+	int ampersand;
+	int flag;
+};
+
